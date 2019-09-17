b35495d8c6167d471aabcd3b9d286864c913511f
Gabi <glafkv@mail.umsl.edu>
2019-09-17 10:29:07 -0500

    Accidentally deleted project. Had to retype it all

--- logParse.c
+++ logParse.c
@@ -21,34 +21,32 @@ bool isSubsetSum(int set[], int n, int sum){
 }
 int main(int argc, char **argv)
 {
-
 	//File and getopt variables
-	int option; 
-	char *inFile;
-	char *outFile;
-	
-	//Default values if not given
+	int option;
+	char *inFile, *outFile;
+	//Defaulst values if not given
 	inFile = "input.dat";
 	outFile = "output.dat";
 	
-	/*int set[] = {3, 24, 4, 12, 5, 2};
-	int sum = 9; 
-	int n = sizeof(set) / sizeof(set[0]);
-	if(isSubsetSum(set, n, sum) == true)
-		printf("Found a subset with given sum\n");
-	else
-		printf("No subset with given sum");*/
+	/*int set[] = {3,24,4,12,5,2};
+ * 	int sum = 9;
+ * 	int n = sizeof(set) / sizeof(set[0]);
+ * 	if(isSubsetSum(set,n,sum) == true)
+ * 		printf("Found a subset with given sum\n");
+ * 	else
+ * 		printf("No subset with given sum");*/
+	
 	//getopt statement
 	while((option = getopt(argc, argv, "hi:o:t:")) != -1)
 	{
 		switch(option){
-		
+			
 			case 'h':
-				printf("\tI'm here to help!\n");
-				printf("**************************\n");
+				printf("\tI'm here to help\n");
+				printf("***********************\n");
 				printf("-h brings up the help menu\n");
-				printf("-i allows you to add your own input file. Default is 'input.dat'\n");
-				printf("-o allows you to add your own output file. Default is 'output.dat'\n");
+				printf("-i allows you to specify your own input file. Default is 'input.dat'\n");
+				printf("-o allows you to specify your own output file. Default is 'output.dat'\n");
 				printf("-t allows you to specify the maximum duration the code should run. Default is 10 seconds\n");
 				exit(0);
 			case 'i':
@@ -58,31 +56,27 @@ int main(int argc, char **argv)
 				outFile = optarg;
 				break;
 			//case 't':
-				
+			
 			default:
 				printf("Error\n");
 				exit(EXIT_FAILURE);
 		}
 	}
-	
-
-	//Code to open the files
+	//Code to open files
 	FILE *ifPtr = fopen(inFile, "r");
-	FILE *ofPtr = fopen(outFile, "a");
-
-	//if the input file doesn't exist, throw an error.
+	FILE *ofPtr = fopen(outFile, "w");
+	
+	//if input file doesn't exist, throw an error.
 	if(ifPtr == NULL){
+		perror("logParse.c: Error: ");	
+		exit(EXIT_FAILURE);
+	}
+	//if output file doens't exist, throw an error
+	if(ofPtr == NULL){
 		perror("logParse.c: Error: ");
 		exit(EXIT_FAILURE);
 	}
-	
-	//if the output file doesn't exist, throw an error.
-	if(ofPtr == NULL){
-		perror("logParse.c: Error: ");
-		exit(EXIT_FAILURE);	
-	}
-
-	//Variables to get the numbers from the files.
+	//variables to get the numbers from the files
 	char str[60];
 	const char s[2] = " ";
 	char *token;
@@ -93,46 +87,42 @@ int main(int argc, char **argv)
 	//Flag for child
 	int counter;
 	int set[100];
-
-	//Gets the first line of the input file
+	
+	//gets the first line of the input file
 	fgets(str, 60, ifPtr);
 	
 	token = strtok(str, s);
-	//printf("%s\n", token);
-	//Converts the first number to an int to be processed by the for loop
+	//converts the first number to an int to be processed by for loop
 	num = atoi(token);
 	
-	//pid string coverting variables
+	//PID string converting variables
 	char pidString[10];
 	char endString[100];
 	
 	//parent and child variables
 	int pid, status, childPID, fNum;
 	
-
-	
-	//Goes into loop based on the first number grabbed.
+	//Goes into loop based on the first number grabbed
 	for(i = 0; i < num; i++){
-		//Gets second line of input.
+		//Gets second line of input
 		fgets(str, 60, ifPtr);
 		pid = fork();
-		//if the fork fails
+		//if fork fails
 		if(pid < 0){
 			perror("logParse.c: Error: ");
 		}
-		else if(pid != 0){  //parent
+		else if(pid != 0){	//parent
 			fgets(str, 60, ifPtr);
-			//suspends execution of the current process until a child specified by pid argument has changed state
+			//suspends execution of current process until a child specified by pid argument has changed state
 			waitpid(pid, &status, 0);
-			
-			//For printing all child pids
+			//for printing all child pids
 			sprintf(pidString, "%d ", pid);
 			strcat(endString, pidString);
 		}
-		else{   //Child
-			fprintf(ofPtr, "%d ", getpid());
+		else {	//child
+			//fprintf(ofPtr, "%d ", getpid());
 			counter = 0;
-		
+			
 			sum = strtok(str, s);
 			cNum = atoi(sum);
 			
@@ -140,36 +130,35 @@ int main(int argc, char **argv)
 			fgets(str, 60, ifPtr);
 			
 			sum = strtok(str, s);
-			//Here is where we'll put the subset code.
-			printf("%s\n", sum);
-			
-			/*Need to figure out:
-				How to take in the line, throw it into an array, so we can use it for the subset function.
-				Then print PID and whether or not it's a subset*/	
+			//here is where we'll put the subset code
+			printf("%s\n", sum);	
+
+			/*Need to figure out how to take in a line, throw it into an array so we can use it for the subset function.*/
 			while(counter < cNum){
-				
-				//fscanf(ifPtr, "%d ", &set
+				//fscanf(ifPtr, "%d ", &set);
 				fNum = atoi(sum);
-			//	set[fNum];
+				//set[fNum];
 				//printf("%s \n", sum);
-					
 				counter++;
-				sum = strtok(NULL, s);
+				sum = strtok(NULL,s);
 				printf("%s ", sum);
-			//	printf("%d\n", set[fNum]);
+				//printf("%d\n", set[fNum]);
 			}
-			///printf("%d\n", set[fNum]);
+			//printf("%d\n", set[fNum]);
 			fprintf(ofPtr, "\n");
 			exit(1);
 		}
 	}
-	fprintf(ofPtr, "All children were: %s\n", endString);
+	fprintf(ofPtr, "All the children were: %s\n", endString);
 	fprintf(ofPtr, "Parent PID: %d\n", getpid());
 	
-
 	//Don't forget to close your files!
 	fclose(ofPtr);
 	fclose(ifPtr);
 	exit(EXIT_SUCCESS);
 return 0;
-}	
+}
+			
+
+
+	
--- makefile
+++ makefile
@@ -1,9 +1,23 @@
-logParse:	logParse.o
-		gcc -o logParse logParse.o
 
-logParse.o:	logParse.c
-		gcc -g -c logParse.c
+THIS DOESNT WORK
 
-.PHONY: 	clean
+CC	= gcc
+CFLAGS	= -g
+TARGET	= logParse
+OBJS	= logParse.o
+LIB	= logParse.c
+$(TARGET):	$(OBJS)
+		$(CC) -o $@ $(TARGET) $(OBJS)
+
+$(OBJS):	$(LIB)
+		$(CC) -g -c $(LIB)
+
+.c.o:	
+	$(CC) $(CFLAGS) -c $<
+
+.PHONY: clean
 clean:
-		/bin/rm -f output.dat *.o *~ apppel.2
+	/bin/rm -f *.o *~ $(LIB) $(TARGET)
+
+
+
