9aa9aa4bc6fcf6b7e3e490a98731a6b98e5c9c1b
Gabi <glafkv@mail.umsl.edu>
2019-09-17 19:55:38 -0500

    Need to talk to Bhatia about why it's not handling the subset function

--- logParse.c
+++ logParse.c
@@ -22,6 +22,7 @@ bool isSubsetSum(int set[], int n, int sum){
 	
 	return isSubsetSum(set, n-1, sum) || isSubsetSum(set, n-1, sum-set[n-1]);
 }
+
 int main(int argc, char **argv)
 {
 	//File and getopt variables
@@ -34,14 +35,14 @@ int main(int argc, char **argv)
 
 	//Literally all I have to do is figure out how to put the numbers into an array
 
-	/*int set[] = {3,24,4,12,5,2};
- * 	int sum = 9;
- * 	int n = sizeof(set) / sizeof(set[0]);
- * 	if(isSubsetSum(set,n,sum) == true)
- * 		printf("Found a subset with given sum\n");
- * 	else
- * 		printf("No subset with given sum");*/
-	
+	/*int arr[] = {3,24,4,12,5,2};
+  	int sum = 9;
+  	int o = sizeof(arr) / sizeof(arr[0]);
+  	if(isSubsetSum(arr,o,sum) == true)
+  		printf("Found a subset with given sum\n");
+  	else
+  		printf("No subset with given sum");
+	*/
 	//getopt statement
 	while((option = getopt(argc, argv, "hi:o:t:")) != -1)
 	{
@@ -94,7 +95,7 @@ int main(int argc, char **argv)
 	//Flag for child
 	int counter;
 	int set[100];
-	int k = 0;
+	//int k = 0;
 	int n;
 	
 	//gets the first line of the input file
@@ -110,35 +111,18 @@ int main(int argc, char **argv)
 	
 	//parent and child variables
 	int pid, status, childPID, fNum;
-	
+	//int x = sizeof(set) / sizeof(set[0]);
 	//Goes into loop based on the first number grabbed
 	for(i = 0; i < num; i++){
 		//Gets second line of input
 		fgets(str, 60, ifPtr);
-		/*cToken = strtok(str, s);
-		cNum = atoi(cToken);
-		printf("%d\n", cNum);
-		while(fgets(str, 60, ifPtr) != NULL){
-			printf("the line is: %s\n", cToken);
-		
-		}*/
-		//printf("First num: %d\n", str[1]);
-		
-		/*for(k = 0; k < strlen(str); k++){
-			set[k];
-			printf("Array: %d\n", set[k]);
-		}*/
-		//This will print the whole line. 
-		//Need to figure out how to take this line and put it into an array.
-
-		//printf("%s\n", str);
 		pid = fork();
 		//if fork fails
 		if(pid < 0){
 			perror("logParse.c: Error: ");
 		}
 		else if(pid != 0){	//parent
-		//	fgets(str, 60, ifPtr);
+	
 			//suspends execution of current process until a child specified by pid argument has changed state
 			waitpid(pid, &status, 0);
 		
@@ -148,103 +132,48 @@ int main(int argc, char **argv)
 		}
 		else {	//child
 			fprintf(ofPtr, "%d ", getpid());
-			counter = 0;
-			//pToken = scanf("%d", &str);
-			//printf("%s\n", pToken);
-			//printf("%s\n", str);
-			//This gets the first number. Stops when it gets to " " 
+			fprintf(ofPtr, "\n");
+			//counter = 0;
 			
-			cToken = strtok(str, s);
-			//printf("%s\n", str);	
+			//parsing the first number of the line
+			//cNum is the sum we're trying to get
+			cToken = strtok(str, s);	
 			cNum = atoi(cToken);
-			
-			//fgets(str, 60, ifPtr);
-			//printf("the line is: %s\n", cToken);
 			printf("%d \n", cNum);
-			//while(counter < cNum){
-			//	fNum = atoi(cToken);
-			//	printf("%d ", fNum);
-			//	counter++;
-			//	cToken = strtok(NULL, s);
-			//}
-			//	printf("%s ", cToken);
-			//printf("\n");
 			
 			//THIS IS HUGE
 			//TAKES IN THE REMAINDER OF THE STRING AND PUTS INTO AN ARRAY THEN PRINTS ARRAY
 			printf("Array: ");
+			//int x = sizeof(set)/sizeof(set[0]);
 			while(cToken = strtok(NULL, s)){
 				set[n] = atoi(cToken);
-				
-				//set[n];
-				//n++;
 				printf("%d ", set[n]);
 			}
+			
 			printf("\n");
 			
-			//printf(" %s\n", str);
-			//while(fgets(str, 60, ifPtr) != NULL){
-			//	printf("the line is: %s\n", cToken);
-			//}
-			/*printf("%d\n", cNum);
-			while(fgets(str, 60, ifPtr) != '\0'){
-				set[i] = n;
-				i++;
-			}
 		
-			for(i = 0; i < n; ++i){
-				printf(" %s\n", set[i]);
-			}*/
-			//printf("%s\n", cToken);
-			/*while(fgets(*set[i], 60, ifPtr)){
-				set[i] = '\0';
-				i++;
-			}
-			n = i;
-			for(i = 0; i < n; ++i){
-				printf(" %s\n", set[i]);
-			}
-			printf("\n");*/
-			//this while loop prints all the numbers in the line
-			/*while(fscanf(ifPtr, "%d", &n) > 0){
-				set[k] = n;
-				k++;
-				printf("%d ", n);
-			}
-			printf("\n");*/
-			//printf("%d\n", n);	
-			/*while(counter < cNum){
-				fNum = atoi(cToken);
-				printf("%s ", cToken);
-				counter++;
-				cToken = strtok(NULL,s);
-			}*/
-			/*Keep just in case we need it for the while loop:
- * 			fgets(str, 60, ifPtr);
 			
-			cToken = strtok(str, s);*/
-			//here is where we'll put the subset code
-	
 		
-				/*Need to figure out how to take in a line, throw it into an array so we can use it for the subset function.*/
-			//while(counter < cNum){
-				//fscanf(ifPtr, "%d ", &set);
-			//	fNum = atoi(sum);
-				//set[fNum];
-				//printf("%s \n", sum);
-			//	counter++;
-			//	sum = strtok(NULL,s);
-				//printf("%s ", sum);
-				//printf("%d\n", set[fNum]);
-			//}
-			//printf("%d\n", set[fNum]);
-			fprintf(ofPtr, "\n");
+			int x = sizeof(set)/sizeof(set[0]);
+			/*if(isSubsetSum(set, x - 1, cNum))
+				printf("yes\n");
+			else
+				printf("no\n");
+			*/	
+			/*if(isSubsetSum(set, x, cNum) == true){
+				//printf("%d ", set[n]);
+				printf("Found a subset\n");
+			}else{
+				printf("no\n");
+			}*/
+			
+			
 			exit(1);
-			
 		}
 		
 	}
-
+	fprintf(ofPtr, "\n");
 	fprintf(ofPtr, "All the children were: %s\n", endString);
 	fprintf(ofPtr, "Parent PID: %d\n", getpid());
 	
